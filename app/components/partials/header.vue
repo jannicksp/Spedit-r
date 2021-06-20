fd9f7b06b0a51c35d08943529226718529cf780f
Jannick S. Pedersen <44465341+jannicksp@users.noreply.github.com>
2021-06-20 12:16:08 +0200

    Update header.vue

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -2,25 +2,12 @@
   <header class="flex items-center py-4 md:py-8">
     <div class="header__logo">
       <nuxt-link to="/">
-        <img :src="logo" alt="Logo" />
+        <img class="logohead" :src="logo" alt="Logo" />
       </nuxt-link>
     </div>
 
     <nav class="nav ml-auto">
       <ul class="flex flex-row items-center sm:mt-4 sm:pt-4 md:mt-0 md:pt-0 md:mr-4 lg:mr-8">
-        <li>
-          <nuxt-link to="/blog" class="block font-medium px-4 py-1 md:p-2 lg:px-4">
-            Blog
-          </nuxt-link>
-        </li>
-
-        <li
-          v-for="(page, index) in pages"
-          :key="index"
-          class="block font-medium px-4 py-1 md:p-2 lg:px-4"
-        >
-          <nuxt-link :to="`/${page.slug}`">{{ page.title }}</nuxt-link>
-        </li>
       </ul>
     </nav>
   </header>
@@ -41,6 +28,9 @@ export default class Header extends Vue {
 </script>
 
 <style lang="scss">
+.logohead {
+width: 150px;
+}
 .nav {
   ul li > a {
     &.nuxt-link-active {
