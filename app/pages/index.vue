fe460e8ef94576418dc0300a99d543df9733ed67
Jannick S. Pedersen <44465341+jannicksp@users.noreply.github.com>
2021-06-20 12:18:56 +0200

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,17 +3,7 @@
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
-   
-        <ul> 
-          <li> - Port Kode = 1234 + firkant </li> <br>
-          <li> - Din tomme trailer må ALDRIG stilles i port 5 (Vareindlevering). </li><br>
-          <li> - Vores tomme trailere må stilles i Port 1-7 og den læssede trailer står som regel i port 3. </li><br>
-          <li> - Kontaktperson: Lagermand Børge TLF. 20126157. Kan kontaktes vedrørende manglendee følgesedler og hjælp ved læsning.</li><br>
-          <li> - Vi kører både stykgods og full loads for denne kunde. Ring hvis du er i tvivl om hvad du skal med din trailer.</li><br>
-          <li> - Ved stykgods: Kør til Frejas Terminal Ørstedsvej 11, 8660 Skanderborg og HUSK at melde traileren til på terminalkontoret. </li><br>
-          <li> - Trailertype: Denne kunde bruger trailer med og uden lift + Box trailer. </li><br>
-        </ul>
-
+  
       </div>
       <div class="flex flex-col w-full xl:w-2/5">
         <img
@@ -22,6 +12,17 @@
           src="@/static/images/uploads/rute.PNG"
         />
       </div>
+      
+      <h1> Information</h1>
+       <ul> 
+          <li> - Port Kode = 1234 + firkant </li> <br>
+          <li> - Din tomme trailer må ALDRIG stilles i port 5 (Vareindlevering). </li><br>
+          <li> - Vores tomme trailere må stilles i Port 1-7 og den læssede trailer står som regel i port 3. </li><br>
+          <li> - Kontaktperson: Lagermand Børge TLF. 20126157. Kan kontaktes vedrørende manglendee følgesedler og hjælp ved læsning.</li><br>
+          <li> - Vi kører både stykgods og full loads for denne kunde. Ring hvis du er i tvivl om hvad du skal med din trailer.</li><br>
+          <li> - Ved stykgods: Kør til Frejas Terminal Ørstedsvej 11, 8660 Skanderborg og HUSK at melde traileren til på terminalkontoret. </li><br>
+          <li> - Trailertype: Denne kunde bruger trailer med og uden lift + Box trailer. </li><br>
+        </ul>
     </div>
   </section>
 </template>
